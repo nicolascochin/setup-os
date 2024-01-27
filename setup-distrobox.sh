4885c3d8e09ba30821319ddc7eb95a0214545aaa
Nicolas COCHIN <nicolas.cochin@lifen.fr>
2024-01-27 22:09:50 +0100

    Update setup-distrobox.sh

--- setup-distrobox.sh
+++ setup-distrobox.sh
@@ -4,33 +4,26 @@
 # $2: name of the distrobox
 
 IMAGE=$(test -z $1 && echo "fedora" || echo $1)
+NAME=$(test -z $2 && echo "$IMAGE" || echo $2)
 DISTRO_SCRIPT_URL="https://raw.githubusercontent.com/nicolascochin/setup-os/main/distrobox/$IMAGE.sh"
 
+fn_exists() { declare -F "$1" > /dev/null; }
+
 if curl --output /dev/null --silent --head --fail "$DISTRO_SCRIPT_URL"; then
+  echo "Fetching custom distrobox file"
   source <(curl -s "$DISTRO_SCRIPT_URL")
 fi
 
+PACKAGES_TO_INSTALL=${PACKAGES[@]:-""}
 
-#declare -A PACKAGES=(
-#  ["fedora"]="zsh gh neovim bat fzf jq git tmux tmate"
-#)
-
-#PACKAGES_TO_INSTALL=$(! [ "${PACKAGES[$IMAGE]+foo}"  ] && echo "" || echo "${PACKAGES[$IMAGE]}")
-#PACKAGES_TO_INSTALL=$(! [ "${PACKAGES[$IMAGE]+foo}"  ] && echo "Image '$IMAGE' is unknown. Exiting..." && exit 1)
-
+echo "Installing distrobox with name: $NAME"
 echo "Installing distrobox with image: $IMAGE"
 echo "Packages: >${PACKAGES_TO_INSTALL}<"
-
-custom_install "foo"
-exit
-CUSTOM_HOME=$HOME/Distroboxes/$IMAGE
 
 distrobox create  \
   --image $IMAGE \
   --additional-packages "$PACKAGES_TO_INSTALL"
 
-#  --home $CUSTOM_HOME \
-#ln -s $HOME $CUSTOM_HOME/bip
-
 distrobox enter $IMAGE -- chsh -s /bin/zsh
+fn_exists custom_install && custom_install "$NAME"
 distrobox enter $IMAGE
