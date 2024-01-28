e1ba5f316bdfedcf0c36c71c3083ef46aa96eab0
Nicolas COCHIN <nicolas.cochin@lifen.fr>
2024-01-28 23:05:54 +0100

    Update setup-distrobox.sh

--- setup-distrobox.sh
+++ setup-distrobox.sh
@@ -7,8 +7,26 @@ IMAGE=$(test -z $1 && echo "fedora" || echo $1)
 NAME=$(test -z $2 && echo "$IMAGE" || echo $2)
 DISTRO_SCRIPT_URL="https://raw.githubusercontent.com/nicolascochin/setup-os/main/distrobox/$IMAGE.sh"
 
+## Functions
 fn_exists() { declare -F "$1" > /dev/null; }
 
+# $1 == title
+do_we_continue() {
+  while
+    echo $1
+    echo -n "Do you want to continue? (y|n)"
+    read RESPONSE
+    ! echo ${RESPONSE} | egrep -q "^y|n$"
+  do true; done
+  if [[ ${RESPONSE} == "y" ]]; then
+    return
+  fi
+  echo "skipping"
+  false
+}
+
+
+## Script
 if curl --output /dev/null --silent --head --fail "$DISTRO_SCRIPT_URL"; then
   echo "Fetching custom distrobox file"
   source <(curl -s "$DISTRO_SCRIPT_URL")
@@ -19,6 +37,7 @@ PACKAGES_TO_INSTALL=${PACKAGES[@]:-""}
 echo "Installing distrobox with name: $NAME"
 echo "Installing distrobox with image: $IMAGE"
 echo "Packages: >${PACKAGES_TO_INSTALL}<"
+! do_we_continue && echo "Exiting..." && echo 1
 
 distrobox create  \
   --image $IMAGE \
