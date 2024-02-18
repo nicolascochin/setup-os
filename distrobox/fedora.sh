3af2babab24a72c15787b51e712f965c723a2f14
Nicolas COCHIN <nicolas.cochin@lifen.fr>
2024-02-18 16:10:44 +0100

    Update fedora.sh

--- distrobox/fedora.sh
+++ distrobox/fedora.sh
@@ -15,6 +15,11 @@ PACKAGES=(
 
 # $1: name of the distrobox
 custom_install() {
-  distrobox enter $1 -- sudo ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
-  distrobox enter $1 -- sudo ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
+  echo "Link flatpak to host" && distrobox enter $1 -- sudo ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
+  echo "Link podman to host" && distrobox enter $1 -- sudo ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
+
+  echo "Install Plug for Neovim" && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  echo "Install Copilot for Neovim" && git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
+
+  echo "Install Tmplux plugins" && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
