ba2a59a7946f8714fc061eff1c7b8a4b98564d71
Nicolas COCHIN <nicolas.cochin@lifen.fr>
2024-02-22 16:56:20 +0100

    Update fedora.sh

--- distrobox/fedora.sh
+++ distrobox/fedora.sh
@@ -22,5 +22,5 @@ custom_install() {
   echo "Install Plug for Neovim" && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   echo "Install Copilot for Neovim" && git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
 
-  echo "Install Tmplux plugins" && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  echo "Install Tmux plugins" && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
