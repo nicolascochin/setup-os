cd12817a0e2172864282b19ff6de2e58a885b52f
Nicolas COCHIN <github@account.rocks>
2024-03-19 23:22:37 +0100

    test

--- distrobox/docker-ubuntu.sh
+++ distrobox/docker-ubuntu.sh
@@ -37,24 +37,26 @@ post_install() {
   setup_docker
 }
 
-alias enter_distrobox="distrobox enter --root $NAME"
+enter_distrobox() {
+  distrobox enter --root $NAME
+}
 
 setup_docker() {
-  enter_distrobox -- sudo install -m 0755 -d /etc/apt/keyrings
-  enter_distrobox -- sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  enter_distrobox -- sudo chmod a+r /etc/apt/keyrings/docker.asc
+  distrobox enter --root $NAME -- sudo install -m 0755 -d /etc/apt/keyrings
+  distrobox enter --root $NAME -- sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  distrobox enter --root $NAME -- sudo chmod a+r /etc/apt/keyrings/docker.asc
 
   echo "Add the repository to Apt sources..."
-  enter_distrobox -- echo \
+  distrobox enter --root $NAME -- echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  enter_distrobox -- sudo apt-get update
+  distrobox enter --root $NAME -- sudo apt-get update
 
   echo "Install Docker..."
-  enter_distrobox -- sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  distrobox enter --root $NAME -- sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
   echo "Enable Docker..."
-  enter_distrobox -- sudo systemctl enable --now docker
-  enter_distrobox -- sudo usermod -aG docker $USER
+  distrobox enter --root $NAME -- sudo systemctl enable --now docker
+  distrobox enter --root $NAME -- sudo usermod -aG docker $USER
 }
