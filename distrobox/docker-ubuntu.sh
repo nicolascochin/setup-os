c477e85cfe744560e2084ddbda654206739c7888
Nicolas COCHIN <github@account.rocks>
2024-03-19 23:19:14 +0100

    try with alias

--- distrobox/docker-ubuntu.sh
+++ distrobox/docker-ubuntu.sh
@@ -37,24 +37,24 @@ post_install() {
   setup_docker
 }
 
-enter_distrobox() {
-  distrobox enter --root $NAME
-}
+alias enter_distrobox="distrobox enter --root $NAME"
 
 setup_docker() {
-  distrobox enter --root $NAME -- \
-    sudo install -m 0755 -d /etc/apt/keyrings \
-    && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc \
-    && sudo chmod a+r /etc/apt/keyrings/docker.asc \
-    && echo "Add the repository to Apt sources..." \
-    && echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  enter_distrobox -- sudo install -m 0755 -d /etc/apt/keyrings
+  enter_distrobox -- sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  enter_distrobox -- sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  echo "Add the repository to Apt sources..."
+  enter_distrobox -- echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
-    && sudo apt-get update \
-    && echo "Install Docker..." \
-    && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-    && echo "Enable Docker..." \
-    && sudo systemctl enable --now docker \
-    && sudo usermod -aG docker $USER
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  enter_distrobox -- sudo apt-get update
+
+  echo "Install Docker..."
+  enter_distrobox -- sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  echo "Enable Docker..."
+  enter_distrobox -- sudo systemctl enable --now docker
+  enter_distrobox -- sudo usermod -aG docker $USER
 }
