c472485f0a5c3cdfa6b8ebfefc5d303e277094d4
Nicolas COCHIN <github@account.rocks>
2024-03-21 16:25:16 +0100

    Refactor + fix

--- distrobox/docker-debian.sh
+++ distrobox/docker-debian.sh
@@ -29,27 +29,9 @@ create_distrobox() {
 
 post_install() {
   setup_nvim_and_tmux
-  install_docker
+  install_docker "debian"
 }
 
 enter_distrobox() {
   distrobox enter --root $NAME "$@"
-}
-
-
-install_docker() {
-  enter_distrobox -- sudo install -m 0755 -d /etc/apt/keyrings
-  enter_distrobox -- sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  enter_distrobox -- sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-  # Add the repository to Apt sources:
-  enter_distrobox -- sh -c "echo 'deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo "\$VERSION_CODENAME") stable' | xargs -I % sh -c 'echo %' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
-  enter_distrobox -- sudo apt-get update
-
-  # Install Docker
-  enter_distrobox -- sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-  # Enable Docker
-  enter_distrobox -- sudo systemctl enable --now docker
-  enter_distrobox -- sudo usermod -aG docker $USER
 }
