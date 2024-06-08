96e0f53e9ef08fc680f6c8e5e1919b2c52ae136c
Nicolas COCHIN <github@account.rocks>
2024-06-08 12:50:30 +0200

    Update fedora.sh

--- distrobox/fedora.sh
+++ distrobox/fedora.sh
@@ -4,6 +4,7 @@ create_distrobox() {
   distrobox create  \
     --image fedora:$VERSION \
     --name $NAME \
+    --pre-init-hooks "sh -c 'echo -e \"[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\" > /etc/yum.repos.d/vscode.repo'" \
     --additional-packages "$PACKAGES_TO_INSTALL"
 }
 
