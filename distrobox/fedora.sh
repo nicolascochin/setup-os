1a80d00ecdf3e32b177567c998d445202c9be6c2
Nicolas COCHIN <github@account.rocks>
2024-06-09 11:01:58 +0200

    Update fedora.sh

--- distrobox/fedora.sh
+++ distrobox/fedora.sh
@@ -14,7 +14,7 @@ enter_distrobox() {
 install_vsc() {
   echo "Install VSC"  
   enter_distrobox -- sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-  enter_distrobox -- sudo dnf install code
+  enter_distrobox -- sudo dnf install -y code
 }
 
 post_install() {
