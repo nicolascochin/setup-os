1e8b85ab49b1b510389983aa71880577034680f8
Nicolas COCHIN <github@account.rocks>
2024-06-26 22:36:55 +0200

    Update common.sh

--- distrobox/common.sh
+++ distrobox/common.sh
@@ -1,7 +1,7 @@
 
 setup_nvim_and_tmux() {
   # Plug
-  enter_distrobox -- sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  enter_distrobox -- sh -c 'curl -fLos "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   # Copilot 
   enter_distrobox -- sh -c 'git clone -q https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim'
   # Tmux
