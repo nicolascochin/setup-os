2b2b647ca53cbfb0cda93b3ee22df6d25f853b5f
Nicolas COCHIN <github@account.rocks>
2024-06-28 12:25:24 +0200

    Update common.sh

--- distrobox/common.sh
+++ distrobox/common.sh
@@ -8,6 +8,10 @@ setup_nvim_and_tmux() {
   enter_distrobox -- sh -c 'git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'
 }
 
+install_nvm() {
+  enter_distrobox -- sh -c "test -d ${HOME}/.nvm && curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
+}
+
 install_host_exec() {
   echo "Link flatpak to host"  && enter_distrobox -- sudo ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
   echo "Link podman to host"   && enter_distrobox -- sudo ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
