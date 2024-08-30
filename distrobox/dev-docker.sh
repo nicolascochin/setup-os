715a0b8f4477b8f0c95086f4748d2e5cff51fdcd
Nicolas COCHIN <github@account.rocks>
2024-08-30 14:41:47 +0200

    Update dev-docker.sh

--- distrobox/dev-docker.sh
+++ distrobox/dev-docker.sh
@@ -42,10 +42,9 @@ enter_distrobox() {
 
 install_vscode() {
   enter_distrobox -- sudo apt-get install wget gpg
-  enter_distrobox -- wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-  enter_distrobox -- sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  enter_distrobox -- wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
+  enter_distrobox -- sudo install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
   enter_distrobox -- echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-  enter_distrobox -- rm -f packages.microsoft.gpg
   enter_distrobox -- sudo apt install apt-transport-https
   enter_distrobox -- sudo apt update
   enter_distrobox -- sudo apt install code
