0cd5a3cc80b3fe84be5ed8a4cbf2797372c27944
Nicolas COCHIN <github@account.rocks>
2024-08-30 10:58:13 +0200

    Update dev-docker.sh

--- distrobox/dev-docker.sh
+++ distrobox/dev-docker.sh
@@ -15,7 +15,6 @@ create_distrobox() {
     --volume ${HOME}/Workspace:${HOME}/distroboxes/${NAME}/Workspace:rw \
     --additional-packages "$PACKAGES_TO_INSTALL" \
     --init \
-    --init-hooks "sudo sed -i \"s/^#Port 22/Port $PORT/\" /etc/ssh/sshd_config && sudo systemctl enable ssh" \
     --unshare-all
 }
 
@@ -28,24 +27,28 @@ post_install() {
   echo
   install_nvm
   echo
-  echo "Install Docker"
-  install_docker
-  echo  
-  echo "Config SSH"
-  cat <<EOF >> ~/.ssh/config
-Host $NAME
-  User nico
-  Port $PORT
-  HostName localhost
-EOF
+  echo "Install VSC" && install_vscode
+  echo 
+  echo "Install Docker" && install_docker
   echo
-  install_gh_and_ssh
+  echo "Login to Github" && enter_distrobox -- gh auth login
   echo  
   echo "Enter distrobox and setup config-files project"
 }
 
 enter_distrobox() {
   distrobox enter --root --no-workdir --clean-path $NAME "$@"
+}
+
+install_vscode() {
+  enter_distrobox -- sudo apt-get install wget gpg
+  enter_distrobox -- wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  enter_distrobox -- sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  enter_distrobox -- echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+  enter_distrobox -- rm -f packages.microsoft.gpg
+  enter_distrobox -- sudo apt install apt-transport-https
+  enter_distrobox -- sudo apt update
+  enter_distrobox -- sudo apt install code
 }
 
 install_docker() {
